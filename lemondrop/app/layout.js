b03df1c32de3487a0cca60611e2f7604ea794343
sumanth37z <sumanth7077@gmail.com>
2026-03-26 07:33:46 +0530

    changed fonts and icons

--- lemondrop/app/layout.js
+++ lemondrop/app/layout.js
@@ -1,4 +1,11 @@
 import "./globals.css";
+import { Silkscreen } from "next/font/google";
+
+const silkscreen = Silkscreen({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-display",
+});
 
 export const metadata = {
   title: "LemonDrop - Free Disposable Email",
@@ -7,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={silkscreen.variable}>
       <body className="antialiased">{children}</body>
     </html>
   );
